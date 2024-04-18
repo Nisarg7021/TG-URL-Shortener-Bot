b6d78b024c7f074f84ff682fd31822f0b7fe4e04
Nisarg7021 <95787542+Nisarg7021@users.noreply.github.com>
2024-04-19 00:26:54 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -41,7 +41,20 @@ if __name__ == "__main__" :
         plugins=plugins,
         sleep_threshold=5,
     )
-    
+
+
+class Bot(Client):
+
+    def __init__(self):
+        super().__init__(
+        "shortener",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        bot_token=BOT_TOKEN,
+        sleep_threshold=5,
+        plugins=dict(root="plugins")
+        )
+  
     async def start(self):
         me = await self.get_me()
         self.owner = await self.get_users(int(OWNER_ID))
@@ -90,18 +103,3 @@ if __name__ == "__main__" :
 app = Bot()
 app.run()
 
-#GreyMattersTech = Bot()
-#GreyMattersTech.run()
-"""
-   _____                    __  __         _    _              _       _______           _     
-  / ____|                  |  \/  |       | |  | |            ( )     |__   __|         | |    
- | |  __  _ __  ___  _   _ | \  / |  __ _ | |_ | |_  ___  _ __|/ ___     | |  ___   ___ | |__  
- | | |_ || '__|/ _ \| | | || |\/| | / _` || __|| __|/ _ \| '__| / __|    | | / _ \ / __|| '_ \ 
- | |__| || |  |  __/| |_| || |  | || (_| || |_ | |_|  __/| |    \__ \    | ||  __/| (__ | | | |
-  \_____||_|   \___| \__, ||_|  |_| \__,_| \__| \__|\___||_|    |___/    |_| \___| \___||_| |_|
-                      __/ |                                                                    
-                     |___/                                                                     
-Author: GreyMatter's Tech
-GitHub: https://GreyMattersTech.com/GitHub
-Website: https://GreyMattersTech.com
-"""
